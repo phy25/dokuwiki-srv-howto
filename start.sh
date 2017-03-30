f201d990c51bc859fd03e66173274fde780eb210
phy25 <git@phy25.com>
2017-03-30 16:26:36 +0800

    Nginx is already running with nobody

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ set -e
 
 chown -R nobody /var/www
 
-su -s /bin/sh nobody -c 'php7 /var/www/bin/indexer.php -c'
+# su -s /bin/sh nobody -c 'php7 /var/www/bin/indexer.php -c'
 
 exec /usr/bin/supervisord -c /etc/supervisord.conf
